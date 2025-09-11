85d981100009a26f9be985dc6af97306735bb719
antfr99 <frigug@yahoo.com>
2025-09-11 12:25:59 +0200

    Test changes to movie_quiz.py

--- movie_quiz.py
+++ movie_quiz.py
@@ -10,9 +10,16 @@ except Exception as e:
     IMDB_Ratings = pd.DataFrame()
     Personal_Ratings = pd.DataFrame()
 
+# --- Remove empty/unnamed columns ---
+def clean_unnamed_columns(df):
+    return df.loc[:, ~df.columns.str.contains('^Unnamed')]
+
+IMDB_Ratings = clean_unnamed_columns(IMDB_Ratings)
+Personal_Ratings = clean_unnamed_columns(Personal_Ratings)
+
 # --- Streamlit Page ---
 st.set_page_config(layout="wide")
-st.title("IMDb & Personal Ratings - Raw Tables")
+st.title("IMDb & Personal Ratings - Cleaned Raw Tables")
 
 # --- Show IMDb Ratings ---
 st.write("---")
