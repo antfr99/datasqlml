352ed4e48375bf844408c30798f09abd6cc8f137
antfr99 <frigug@yahoo.com>
2025-09-11 12:20:31 +0200

    Test changes to movie_quiz.py

--- movie_quiz.py
+++ movie_quiz.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import pandas as pd
 
-# --- Load .xlsxs ---
+# --- Load .xlsx files ---
 try:
-    IMDB_Ratings = pd.read_.xlsx("imdbratings.xlsx", encoding='utf-8-sig')
-    Personal_Ratings = pd.read_.xlsx("myratings.xlsx", encoding='utf-8-sig')
+    IMDB_Ratings = pd.read_excel("imdbratings.xlsx")
+    Personal_Ratings = pd.read_excel("myratings.xlsx")
 except Exception as e:
-    st.error(f"Error loading xlsx files: {e}")
+    st.error(f"Error loading Excel files: {e}")
     IMDB_Ratings = pd.DataFrame()
     Personal_Ratings = pd.DataFrame()
 
@@ -21,7 +21,7 @@ if not IMDB_Ratings.empty:
     st.write("Columns:", IMDB_Ratings.columns.tolist())
     st.dataframe(IMDB_Ratings, width="stretch", height=400)
 else:
-    st.warning("IMDb Ratings xlsx is empty or failed to load.")
+    st.warning("IMDb Ratings Excel file is empty or failed to load.")
 
 # --- Show Personal Ratings ---
 st.write("---")
@@ -30,4 +30,4 @@ if not Personal_Ratings.empty:
     st.write("Columns:", Personal_Ratings.columns.tolist())
     st.dataframe(Personal_Ratings, width="stretch", height=400)
 else:
-    st.warning("Personal Ratings xlsx is empty or failed to load.")
+    st.warning("Personal Ratings Excel file is empty or failed to load.")
