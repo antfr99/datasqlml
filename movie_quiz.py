7d9a52ffd22d674aaace3ae58ab84da7868f8e8c
antfr99 <frigug@yahoo.com>
2025-09-26 16:02:27 +0200

    Clean up movie_quiz files: keep main version, remove copies

--- movie_quiz.py
+++ movie_quiz.py
@@ -1371,7 +1371,7 @@ Given movie features (IMDb rating, genre, director, year, votes), the model pred
         predict_df['Predicted Rating'] = model.predict(X_pred)
 
         if not predict_df.empty:
-            st.subheader("🤖 Predicted Ratings for Unseen English-Language Movies with Changed Ratings")
+            st.subheader("🤖 Predicted Ratings for Unseen Movies with Changed Ratings")
             st.dataframe(
                 predict_df[['Title','IMDb Rating','Genre','Director','Rating Difference','Predicted Rating']]
                 .sort_values(by='Predicted Rating', ascending=False)
